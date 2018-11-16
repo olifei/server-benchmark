6c22155d883e17971489291438fb9bd4cd3d57f0
Olifei <1374633989@qq.com>
2018-11-16 11:32:26 +0800

    add a process for error detection
    three processes:
    1. status monitor
    2. error injection
    3. error detection
    
    1 & 2 can be gathered in one process and be in different threads.

--- servertest.c
+++ servertest.c
@@ -43,8 +43,8 @@ int detect_error(FILE *txt, char* timeinfo) {
 }
 
 int main(int argc,char *argv[]) {
-    FILE *paralog, *statlog, *errorlog;
-    pid_t statpid;
+    FILE *paralog, *statlog, *ierrorlog, *derrorlog;
+    pid_t statpid, detectpid;
     time_t rawtime;
     struct tm * timeinfo;
     struct parameters parameter;
@@ -70,13 +70,42 @@ int main(int argc,char *argv[]) {
         randnum[i] = rand();
     }
 
+    // child process for monitoring status
     statpid = fork();
     if(statpid < 0) {
         exit(EXIT_FAILURE);
     }
-
-    // child process
     if(statpid > 0) {
+
+        // grand-child process for errors detection
+        detectpid = fork();
+        if(detectpid < 0) {
+            exit(EXIT_FAILURE);
+        }
+        if(detectpid > 0) {
+            derrorlog = fopen("/var/log/Daemon-sample/derror.log", "a");
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            fflush(derrorlog);
+            while(restError) {
+                paralog = fopen("/var/log/Daemon-sample/para.log", "r");
+                _parameter_deserialize(&parameter, paralog);
+                time(&rawtime);
+                timeinfo = localtime(&rawtime);
+                parameter.detectionNum = detect_error(derrorlog, asctime(timeinfo));
+                _parameter_serialize(&parameter, paralog);
+                fclose(paralog);
+                restError = parameter.errorNum - parameter.injectionNum;
+                if(restError == 0)
+                    break;
+
+                sleep(600); //wait 10 mins
+            }
+            fclose(derrorlog);
+            exit(EXIT_SUCCESS);
+        }
+
+        // monitor status
         statlog = fopen("/var/log/Daemon-sample/stat.log", "a+");
         time(&rawtime);
         timeinfo = localtime(&rawtime);
@@ -93,28 +122,29 @@ int main(int argc,char *argv[]) {
             restError = parameter.errorNum - parameter.injectionNum;
             printf("Child process: rest error is %d\n", restError);
             fflush(stdout);
-            sleep(5); //wait 5 seconds
+            sleep(30); //wait 30 seconds
         }
+
         fclose(statlog);
         exit(EXIT_SUCCESS);
     }
     
-    // main process
-    errorlog = fopen("/var/log/Daemon-sample/error.log", "a");
+    // main process for errors injection
+    ierrorlog = fopen("/var/log/Daemon-sample/ierror.log", "a");
     for(int i= 0; restError > 0; i++) {
         time(&rawtime);
         timeinfo = localtime(&rawtime);
-        inject_error(randnum[i], errorlog, asctime(timeinfo));
+        inject_error(randnum[i], ierrorlog, asctime(timeinfo));
         parameter.injectionNum++;
-        parameter.detectionNum = detect_error(errorlog, asctime(timeinfo));
+        // parameter.detectionNum = detect_error(ierrorlog, asctime(timeinfo));
         restError = parameter.errorNum - parameter.injectionNum;
         paralog = fopen("/var/log/Daemon-sample/para.log", "w");
         _parameter_serialize(&parameter, paralog);
         fclose(paralog);
-        sleep(rand()%10);
+        sleep(rand()%216); // 6h for 100 errors
         printf("Main process: rest error is %d\n", restError);
         fflush(stdout);
     }
-    fclose(errorlog);
+    fclose(ierrorlog);
     exit(EXIT_SUCCESS);
 }
