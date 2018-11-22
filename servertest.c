c283ca6d1212ec6d0d46e199adde8827c2593925
Olifei <1374633989@qq.com>
2018-11-22 19:32:50 +0800

    add workload part and UCE.sh

--- servertest.c
+++ servertest.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <time.h>
 
+#define CE_UCE_RATIO 33
+
 struct parameters {
     int errorNum;
     int injectionNum;
@@ -25,6 +27,21 @@ static void _parameter_deserialize(struct parameters* para, FILE *txt) {
 };
 
 void inject_error(int address, FILE *txt, char* timeinfo) {
+    static numCE = 0;
+    numCE++;
+    if (numCE % CE_UCE_RATIO == 0) { // inject UCE
+        char Ucommand[40] = "bash ./mce/UCE.sh ";
+        char Uaddress_string[15];
+        int Uaddress = rand();
+        sprintf(Uaddress_string, "%d", Uaddress);
+        strcat(Ucommand, Uaddress_string);
+        system(Ucommand);
+        fflush(txt);
+        fprintf(txt, "Injection UCE: the random address is %d, injected at %s\n", Uaddress, timeinfo);
+        fflush(txt);
+        printf("NOW THE numCE IS %d\n", numCE);
+    }
+    // inject CE
     char command[40] = "bash ./mce/CE.sh ";
     char address_string[15];
     sprintf(address_string, "%d", address);
@@ -32,7 +49,7 @@ void inject_error(int address, FILE *txt, char* timeinfo) {
     // printf("Main the command is %s\n", command);
     system(command);
     fflush(txt);
-    fprintf(txt, "Injection: the random address is %d, injected at %s\n", address, timeinfo); 
+    fprintf(txt, "Injection CE: the random address is %d, injected at %s\n", address, timeinfo); 
     fflush(txt);
 }
 
@@ -46,7 +63,7 @@ int detect_error(FILE *txt, char* timeinfo) {
 
 int main(int argc,char *argv[]) {
     FILE *paralog, *statlog, *ierrorlog, *derrorlog;
-    pid_t statpid, detectpid;
+    pid_t statpid, detectpid, workloadpid;
     time_t rawtime;
     struct tm * timeinfo;
     struct parameters parameter;
@@ -65,90 +82,98 @@ int main(int argc,char *argv[]) {
         fclose(paralog);
     }
 
-    int restError = parameter.errorNum - parameter.injectionNum;
-    int randnum[restError];
-    printf("Main process: # of rest error is %d\n", restError);
-    srand(time(0));
-    for(int i = 0; i < restError; i++) {
-        randnum[i] = rand();
+    workloadpid = fork();
+    if(workloadpid > 0) {
+        char *workload = "sh /root/test/Daemon-sample/workload.sh";
+        system(workload);
     }
+    else {
+        int restError = parameter.errorNum - parameter.injectionNum;
+        int randnum[restError];
+        printf("Main process: # of rest error is %d\n", restError);
+        srand(time(0));
+        for(int i = 0; i < restError; i++) {
+            randnum[i] = rand();
+        }
 
-    // child process for monitoring status
-    statpid = fork();
-    if(statpid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    if(statpid > 0) {
-        // grand-child process for errors detection
-        detectpid = fork();
-        if(detectpid < 0) {
+        // child process for monitoring status
+        statpid = fork();
+        if(statpid < 0) {
             exit(EXIT_FAILURE);
         }
-        if(detectpid > 0) {
-            derrorlog = fopen("/var/log/Daemon-sample/derror.log", "a");
-            time(&rawtime);
-            timeinfo = localtime(&rawtime);
-            fflush(derrorlog);
-            while(restError) {
-                paralog = fopen("/var/log/Daemon-sample/para.log", "r");
-                _parameter_deserialize(&parameter, paralog);
+        if(statpid > 0) {
+            // grand-child process for errors detection
+            detectpid = fork();
+            if(detectpid < 0) {
+                exit(EXIT_FAILURE);
+            }
+            if(detectpid > 0) {
+                derrorlog = fopen("/var/log/Daemon-sample/derror.log", "a");
                 time(&rawtime);
                 timeinfo = localtime(&rawtime);
-                parameter.detectionNum = detect_error(derrorlog, asctime(timeinfo));
-                _parameter_serialize(&parameter, paralog);
-                fclose(paralog);
-                restError = parameter.errorNum - parameter.injectionNum;
-                if(restError == 0)
-                    break;
-                sleep(600); //wait 10 mins
+                fflush(derrorlog);
+                while(restError) {
+                    paralog = fopen("/var/log/Daemon-sample/para.log", "r");
+                    _parameter_deserialize(&parameter, paralog);
+                    time(&rawtime);
+                    timeinfo = localtime(&rawtime);
+                    parameter.detectionNum = detect_error(derrorlog, asctime(timeinfo));
+                    _parameter_serialize(&parameter, paralog);
+                    fclose(paralog);
+                    restError = parameter.errorNum - parameter.injectionNum;
+                    if(restError == 0)
+                        break;
+                    sleep(600); //wait 10 mins
+                }
+                fclose(derrorlog);
+                exit(EXIT_SUCCESS);
             }
-            fclose(derrorlog);
-            exit(EXIT_SUCCESS);
-        }
 
-        // monitor status
-        statlog = fopen("/var/log/Daemon-sample/stat.log", "a");
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
-        fprintf(statlog, "start at %spid is %d\n", asctime(timeinfo), getpid());
-        fflush(statlog);
-        while(restError) {
+            // monitor status
+            statlog = fopen("/var/log/Daemon-sample/stat.log", "a");
             time(&rawtime);
             timeinfo = localtime(&rawtime);
-            fprintf(statlog, "alive at %s\n", asctime(timeinfo));
+            fprintf(statlog, "start at %spid is %d\n", asctime(timeinfo), getpid());
             fflush(statlog);
-            paralog = fopen("/var/log/Daemon-sample/para.log", "r");
-            _parameter_deserialize(&parameter, paralog);
-            fclose(paralog);
+            while(restError) {
+                time(&rawtime);
+                timeinfo = localtime(&rawtime);
+                fprintf(statlog, "alive at %s\n", asctime(timeinfo));
+                fflush(statlog);
+                paralog = fopen("/var/log/Daemon-sample/para.log", "r");
+                _parameter_deserialize(&parameter, paralog);
+                fclose(paralog);
+                restError = parameter.errorNum - parameter.injectionNum;
+                printf("Child process: # of rest error is %d\n", restError);
+                fflush(stdout);
+                if(restError == 0)
+                    break;
+                sleep(30); //wait 30 seconds
+            }
+            fclose(statlog);
+            exit(EXIT_SUCCESS);
+        }
+        
+        // main process for errors injection
+        ierrorlog = fopen("/var/log/Daemon-sample/ierror.log", "a");
+        for(int i= 0; restError > 0; i++) {
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            inject_error(randnum[i], ierrorlog, asctime(timeinfo));
+            parameter.injectionNum++;
+            // parameter.detectionNum = detect_error(ierrorlog, asctime(timeinfo));
             restError = parameter.errorNum - parameter.injectionNum;
-            printf("Child process: # of rest error is %d\n", restError);
+            printf("Main process: # of rest error is %d\n", restError);
             fflush(stdout);
+            paralog = fopen("/var/log/Daemon-sample/para.log", "w");
+            _parameter_serialize(&parameter, paralog);
+            fclose(paralog);
             if(restError == 0)
                 break;
-            sleep(30); //wait 30 seconds
+            sleep(rand()%432); // average 100 errors during 6 hours
         }
-        fclose(statlog);
+        fclose(ierrorlog);
         exit(EXIT_SUCCESS);
     }
     
-    // main process for errors injection
-    ierrorlog = fopen("/var/log/Daemon-sample/ierror.log", "a");
-    for(int i= 0; restError > 0; i++) {
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
-        inject_error(randnum[i], ierrorlog, asctime(timeinfo));
-        parameter.injectionNum++;
-        // parameter.detectionNum = detect_error(ierrorlog, asctime(timeinfo));
-        restError = parameter.errorNum - parameter.injectionNum;
-        printf("Main process: # of rest error is %d\n", restError);
-        fflush(stdout);
-        paralog = fopen("/var/log/Daemon-sample/para.log", "w");
-        _parameter_serialize(&parameter, paralog);
-        fclose(paralog);
-        if(restError == 0)
-            break;
-        sleep(rand()%216); // 6h for 100 errors
-    }
-    fclose(ierrorlog);
-    exit(EXIT_SUCCESS);
 }
