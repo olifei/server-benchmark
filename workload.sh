b85ca0f2220a4fee340598381436cc6e94ec098e
Olifei <1374633989@qq.com>
2018-12-30 23:16:13 +0800

    Update workload.sh

--- workload.sh
+++ workload.sh
@@ -1,17 +1,8 @@
-cd ../testfile
-cd linpack
-
-while true
-do
-    ./runme_xeon64
+cd ../testfile/linpack
+./runme_xeon64
     
-    cd ../stream
-    for i in {1..10}
-    do
-        ./stream_avx512_mcmodel_1600M
-    done
-
-    cd ../linpack
-
+cd ../stream
+for i in {1..10}
+do
+    ./stream_avx512_mcmodel_1600M
 done
-
